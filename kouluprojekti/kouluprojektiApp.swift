d7fc52160e0a7fa28ffc5fb434c46b68580e97b7
Eemil Karvonen <eemil.karvonen@gmail.com>
2020-10-10 17:18:25 +0300

    Initial Commit

---
+++ kouluprojekti/kouluprojektiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kouluprojektiApp.swift
+//  kouluprojekti
+//
+//  Created by Eemil Karvonen on 10.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct kouluprojektiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
